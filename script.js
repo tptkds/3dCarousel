60efd2b146e82dc3d69cce4ad5b15ed41abc94cd
KimYouGyoung <tptkds12@gmail.com>
2024-02-11 19:31:22 +0900

    3D 캐러셀 구현 완료

--- script.js
+++ script.js
@@ -4,4 +4,24 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+  const $carousel = get('.carousel');
+  const $prevBtn = get('.prev_button');
+  const $nextBtn = get('.next_button');
+  let yDeg = 0;
+
+  const init = () => {
+    $prevBtn.addEventListener('click', () => {
+      yDeg += 60;
+      console.log(yDeg);
+      $carousel.style.transform = `translateZ(-346px) rotateY( ${yDeg}deg)`;
+      console.log($carousel.style.transform)
+    })
+    
+     $nextBtn.addEventListener('click', () => {
+       yDeg -= 60;
+       $carousel.style.transform = `translateZ(-346px) rotateY( ${yDeg}deg)`;
+    })
+  }
+
+  init();
 })()
